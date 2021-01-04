bf3f7819ea3f917155fc3e3bfd764c091edc42ed
KWAK8573 <mill8573@gmail.com>
2021-01-04 17:25:02 +0900

    검색기능 구현

--- blog/urls.py
+++ blog/urls.py
@@ -35,8 +35,10 @@ urlpatterns = [
     
     # Example /blog/tag/tagname
     path('tag/<str:tag>/',views.TaggedObjectLV.as_view(), name='tagged_object_list'),
+
     
-    
+    # Example /blog/search/
+    path('search/', views.SearchFormView.as_view(), name='search'),
     
     
     
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.views.generic import DayArchiveView, TodayArchiveView
+from django.views.generic import FormView
+from django.conf import settings
+from django.db.models import Q
+from django.shortcuts import render
+
 
 from blog.models import Post
-from django.views.generic.base import TemplateView
+from blog.forms import PostSearchForm
+
+
 
 
 #--- ListView
@@ -62,7 +69,21 @@ class TaggedObjectLV(ListView):
         context['tagname'] = self.kwargs['tag']
         return context
     
+    #-- FormView
+class SearchFormView(FormView):
+    form_class = PostSearchForm
+    template_name = 'blog/post_search.html'
 
+    def form_valid(self, form):
+        searchWord = form.cleaned_data['search_word']
+        post_list = Post.objects.filter(Q(title__icontains=searchWord) |  Q(description__icontains=searchWord) | Q(content__icontains=searchWord)).distinct()
+
+        context = {}
+        context['form'] = form
+        context['search_term'] = searchWord
+        context['object_list'] = post_list
+
+        return render(self.request, self.template_name, context)
     
     
     
